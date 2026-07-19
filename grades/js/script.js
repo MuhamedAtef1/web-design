1c8e37ff34e94a94a89456b88caf89e454e3bdbd
mohamed <muatef677@gmail.com>
2026-07-19 15:21:14 +0300

    task

---
+++ grades/js/script.js
@@ -0,0 +1,11 @@
+var degree = prompt("Enter Your Degree")
+if (degree > -1 && degree < 101 && degree != null && degree != "") {
+    if (degree > 90) alert("Your Grade is A")
+    else if (degree > 90) alert("Your Grade is A")
+    else if (degree > 80) alert("Your Grade is B")
+    else if (degree > 70) alert("Your Grade is C")
+    else alert("Your Grade is F")
+}
+else {
+    alert("Wrong degree")
+}
